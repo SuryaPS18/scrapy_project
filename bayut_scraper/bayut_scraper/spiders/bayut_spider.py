ba8869fa2c171c5c5a0750e109d3399938f0e494
SuryaPS18 <pssurya186@gmail.com>
2023-06-25 20:38:44 +0530

    first commit

---
+++ bayut_scraper/bayut_scraper/spiders/bayut_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+
+class Bayut_spiderSpider(scrapy.Spider):
+    name = "bayut_spider"
+    allowed_domains = ["bayut.com"]
+    start_urls = ["https://www.bayut.com/to-rent/property/dubai/"]
+
+    def parse(self, response):
+        apartments = response.css('article.ca2f5674')
+
+        for apartment in apartments:
+            yield{
+                'url' : apartment.css('._4041eb80 a').attrib['href'],
+            }
+
+        next_page = response.css('._92c36ba1 a ::attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = 'https://www.bayut.com' + next_page
+            yield response.follow(next_page_url,callback=self.parse)
